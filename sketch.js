c6daac0ff6b132638e278d286a0c86d9aadd41d0
core <msbeethoven@users.noreply.github.com>
2020-01-05 22:12:52 -0500

    Carries the sticker outline and places it on the canvas

--- sketch.js
+++ sketch.js
@@ -1,4 +1,10 @@
+let img;
+let layer;
+let col;
+let pos;
 let retro;
+let colors;
+let pickedColor;
 
 function preload() {
   let img = ['./photo_1.jpg', './photo_2.jpg', './photo_3.jpg', './photo_4.jpg'];
@@ -7,14 +13,24 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(720, 700);
-  background(retro);
+  createCanvas(720, 720);
+  layer = createGraphics(720,720);
+  col = color(0,0,0);
+  strokeWeight(1);
+}
+
+function draw() {
+  image(retro, 0, 0);
+  image(layer,0,0);
+  noFill();
+  stroke(col) ;
+  ellipse(mouseX, mouseY, 50);
 }
 
 function mousePressed() {
-  let colors = [color('blue'), color('yellow'), color('green'), color('red')];
-  let pickedColor = random(colors);
-  fill(pickedColor);
-  noStroke();
-  ellipse(mouseX, mouseY, 50, 50);
+  layer.noStroke(); 
+  colors = [color('blue'), color('yellow'), color('green'), color('red')];
+  pickedColor = random(colors);
+  layer.fill( pickedColor ); 
+  layer.ellipse( mouseX, mouseY, 50); 
 }
